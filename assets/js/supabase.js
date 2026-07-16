98895ff64b4301189ac513c176cf27b251f6cdb2
Ele <144879688+Eleee28@users.noreply.github.com>
2026-07-16 21:59:41 +0200

    delete test insert user

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -19,20 +19,3 @@ async function probarConexion() {
 }
 
 probarConexion();
-
-
-async function insertarSocio() {
-
-    const { data, error } = await supabaseClient
-        .from("socios")
-        .insert({
-            nombre: "Juan",
-            apellido: "Pérez",
-            email: "juan@test.com",
-            password_hash: "temporal"
-        });
-
-    console.log(data, error);
-}
-
-insertarSocio();
