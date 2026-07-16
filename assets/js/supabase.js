4ce02b523d20c1a084cd3270f35f0280fd4e7d33
Ele <144879688+Eleee28@users.noreply.github.com>
2026-07-16 21:21:29 +0200

    Create supabase.js

---
+++ assets/js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://lqqqbiltwrmkjmrmpwpu.supabase.co";
+const SUPABASE_KEY = "sb_publishable_ij8DKD_mYxqLxtF1L6PlCA_EZfXE_hc";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
