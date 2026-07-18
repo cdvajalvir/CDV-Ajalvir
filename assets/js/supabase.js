08a4c3c30720b5694d4ec3100e65dc933d919b93
Ele <144879688+Eleee28@users.noreply.github.com>
2026-07-18 15:24:00 +0000

    fix error

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const SUPABASE_URL = "https://lqqqbiltwrmkjmrmpwpu.supabase.co";
 const SUPABASE_KEY = "sb_publishable_ij8DKD_mYxqLxtF1L6PlCA_EZfXE_hc";
 
-const supabaseClient = window.supabase.createClient(
+export const supabaseClient = createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
