dfae978d0970e749ef40bbbff63630a76738c620
Ele <144879688+Eleee28@users.noreply.github.com>
2026-07-18 14:11:38 +0000

    export client

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://lqqqbiltwrmkjmrmpwpu.supabase.co";
 const SUPABASE_KEY = "sb_publishable_ij8DKD_mYxqLxtF1L6PlCA_EZfXE_hc";
 
-const supabaseClient = window.supabase.createClient(
+export const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
