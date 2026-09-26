156f8c41ee09c3a91739adb928298691dd937eb3
cdvajalvir <cdve.ajalvir@gmail.com>
2026-09-26 15:00:14 +0200

    Refactor active status condition in activasocio.js

--- assets/js/activasocio.js
+++ assets/js/activasocio.js
@@ -236,7 +236,9 @@ async function cargarHistoricoTemporada(temporada) {
                     
                     // Si cuota - pagado NO es negativo (es decir, >= 0), se considera cubierto/activo
                     if ((cuota - pagado) >= 0) {
-                        esActivo = true;
+                        if (cuota === 0) {
+                            esActivo = true;
+                        }
                     }
                 }
             }
